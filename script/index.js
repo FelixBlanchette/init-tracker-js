358fd7bcd90eedbdefbbe1663f079a292b9e0bb7
Félix Blanchette <felix.blanchette01@gmail.com>
2025-08-11 13:44:51 -0400

    thats for you LCB

--- script/index.js
+++ script/index.js
@@ -1,10 +1,41 @@
 
+const trackerContainer = document.querySelector(".tracker-container")
+let trackerList = []
+let turnTracker = null
 
-
-let addTrackerOnClick = (nameInput, initCount, lifePoints) => {
+const addTrackerOnClick = (nameInput, initCount, lifePoints) => {
     const t = new Tracker(nameInput, initCount, lifePoints, false, false)
-    // addto tracker list
+    console.log(t)
+    trackerList.push(t)
+    renderTracker()
     return t
+}
+
+const renderTracker = () => {
+    trackerContainer.innerHTML = ""
+
+    trackerList.forEach(tracker => {
+        const card = document.createElement("div")
+        card.classList.add("tracker-card")
+
+        if (tracker._isTurnTracker) card.classList.add("turn")
+        if (tracker._isDead) card.classList.add("dead")
+
+        card.innerHTML = `
+            <h3>${tracker._name}</h3>
+            <p><strong>Initiative:</strong>${tracker._initiativeCount}</p>
+            <p><strong>Life Points:</strong>${tracker._lifePoints}</p>
+            `
+        console.log(trackerContainer)
+        trackerContainer.appendChild(card)
+    })
+}
+
+const nextTurnOnClick = () => {
+    if (turnTracker == null) {
+        turnTracker = trackerList[0]
+        trackerList[0].changeTurnTracker()
+    }
 }
 
 document.getElementById("trackerForm").addEventListener("submit", (e) => {
